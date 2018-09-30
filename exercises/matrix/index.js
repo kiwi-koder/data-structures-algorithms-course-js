833fc5db133eb6058fb20d60e22f8f8b2fc1f1a8
Joshua Rippon <joshuarippon1@gmail.com>
2018-09-30 20:19:54 +0200

    Solved matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,68 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// State
+// 0 -> right
+// 1 -> down
+// 2 -> left
+// 3 -> up
+function matrix(
+    n,
+    array = new Array(n).fill("").map(el => new Array(n).fill("x")),
+    state = 0,
+    col = 0,
+    row = 0,
+    count = 1
+) {
+    if (count > Math.pow(n, 2)) return array;
+    switch (state) {
+        case 0:
+            if (array[row] && array[row][col] === "x") {
+                array[row][col] = count;
+                col++;
+                count++;
+            } else {
+                col--;
+                row++;
+                state = (state + 1) % 4;
+            }
+            return matrix(n, array, state, col, row, count);
+        case 1:
+            if (array[row] && array[row][col] === "x") {
+                array[row][col] = count;
+                row++;
+                count++;
+            } else {
+                col--;
+                row--;
+                state = (state + 1) % 4;
+            }
+            return matrix(n, array, state, col, row, count);
+        case 2:
+            if (array[row] && array[row][col] === "x") {
+                array[row][col] = count;
+                col--;
+                count++;
+            } else {
+                col++;
+                row--;
+                state = (state + 1) % 4;
+            }
+            return matrix(n, array, state, col, row, count);
+        case 3:
+            if (array[row] && array[row][col] === "x") {
+                array[row][col] = count;
+                row--;
+                count++;
+            } else {
+                col++;
+                row++;
+                state = (state + 1) % 4;
+            }
+            return matrix(n, array, state, col, row, count);
+        default:
+            break;
+    }
+}
 
 module.exports = matrix;
