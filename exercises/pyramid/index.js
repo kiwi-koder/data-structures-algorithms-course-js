e17a8272092269aca1604ce1e207d1b810a012dc
Joshua Rippon <joshuarippon1@gmail.com>
2018-09-30 18:49:48 +0200

    Solved pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -10,13 +10,24 @@
 //       ' # '
 //       '###'
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//       '  #  ' 4 white 1 hash
+//       ' ### ' 2 white 3 hash
+//       '#####' 0 white 5 hash
 
 function pyramid(n) {
-    const baseLength = 2 * n - 1;
-    
+    const maxStairLength = 2 * n - 1;
+
+    for (let row = 1; row <= n; row++) {
+        const hashLength = 2 * row - 1;
+        const whiteLength = maxStairLength - hashLength;
+        let stair = "";
+        for (let col = 1; col <= maxStairLength; col++) {
+            if (col <= whiteLength / 2 || col > whiteLength / 2 + hashLength)
+                stair += " ";
+            else stair += "#";
+        }
+        console.log(stair);
+    }
 }
 
 module.exports = pyramid;
